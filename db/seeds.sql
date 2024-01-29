4f39b4d77790d0c21c81ce1bbb8e64ad83ca71fa
WCIII <warrenlathe@me.com>
2024-01-28 15:26:11 -1000

    added database schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO departments (name)
+VALUES  ('Sales'), 
+        ('Engineering'),
+        ('Finance'), 
+        ('Legal');
+
+INSERT INTO roles (title, salary, department_id)    
+VALUES  ('Sales Lead', 100000, 1),
+        ('Salesperson', 80000, 1),
+        ('Lead Engineer', 150000, 2),
+        ('Software Engineer', 120000, 2),
+        ('Accountant', 125000, 3),
+        ('Legal Team Lead', 250000, 4),
+        ('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ('Luke', 'Skywalker', 1, NULL),
+        ('ObiWan', 'Kenobi', 2, 1),
+        ('Princess', 'Leia', 3, NULL),
+        ('Yoda', 'Yoda', 4, 3),
+        ('Han', 'Solo', 5, NULL),
+        ('Darth', 'Vader', 6, 5),
+        ('Chew', 'Bacca', 7, 5),
+        ('R2', 'D2', 8, 7);
