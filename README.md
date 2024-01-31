297f0a5b3d57a3995f2f9012d282305530e49d20
WCIII <warrenlathe@me.com>
2024-01-31 07:49:36 -1000

    deleted node_modules

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# employee-tracker-chal12
-A command line employee database using mySql and database
+# EMPLOYEE TRACKER
+Table of Contents:
+- [User Story/Challenge Goal](#user-storychallenge-goal)
+- [Resources Used & Credits](#resources-user--credits)
+- [Relevant Links](#relevant-links)
+- [Screenshots & Screencasts](#screenshots--screencasts)
+- [Usage and Comments](#usage--comments)
+- [Future Direction and Contributing](#future-directions-and-contributing)
+- [Tests](#tests)
+
+## USER STORY/CHALLENGE GOAL:
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+
+## RESOURCES USED && CREDITS:
+- Class instruction and office hours, Kyle and Drew, specifically for assistance understanding CRUD and equivelent functions  .
+- W3School sections on mysql, express and try/catch, Completed node.js/mysql tutorial
+- Student interactions and questions during class
+- AI helping understanding mysql/promise functionality
+
+
+## RELEVANT LINKS:
+- Repository: https://github.com/TreyLathe/employee-tracker-chal12
+
+- Deployed Site:  N/A
+
+## SCREENSHOTS &&/|| SCREENCASTS:
+Screencast: 
+
+
+## USAGE && COMMENTS:
+
+>GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role, also list all employees by manager.
+
+Options appear as listed above.
+
+>WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+
+Formatted table of department names and ids is shown.
+
+>WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+Table of job title, role id, department and salary are shown.
+
+>WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+Table is shown of all employees including id, names, salaries, and managers is shown.
+
+>WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+
+Choosing the option to add a department prompts user to add new department. New department is then added to the correct table. 
+
+>WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+Choosing the option to add a role prompts user to add new role including name, salary and department. New role is then added to the correct table.
+
+>WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+Choosing the option to add an employee prompts user to add new employee including name, role and manager. New employee is then added to the correct table. Database is updated.
+
+>WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+CHoosing the option to update an employee role, prompts user to choose employe ID and new role ID. Database is updated. 
+
+>WHEN I choose to list employees by manager,
+THEN a table of employees is shown organized by manager.
+
+Choosing this option shows a list of all employees organized by manager. 
+
+## FUTURE DIRECTIONS AND CONTRIBUTING
+
+- Add more functionality   
+    - add more functionality to list data in various manners
+    - add more functionality to modify datat
+    - add web interface functionality 
+- JEST test code
+    
+
+## TESTS
+
+Currently, no Jest tests, console logs help with testing, type node server.js in appropriate directory to test functionality of application.
--- server.js
+++ server.js
@@ -6,20 +6,22 @@ const inquirer = require("inquirer");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'employees_db'
-  }).then((connection) => {
+mysql
+  .createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "employees_db",
+  })
+  .then((connection) => {
     db = connection;
-    console.log(`Connected to the employees_db database.`);
+    console.log(`Connected to employees_db database.`);
     start();
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.error(`Error connecting to the database: ${error.toString()}`);
     process.exit(1);
   });
-  
 
 const start = () => {
   inquirer
@@ -69,168 +71,214 @@ const start = () => {
           break;
 
         case "View Employees by Manager":
-            EmployeesByManager();
-            break;            
+          EmployeesByManager();
+          break;
+        
+        case "View Total Budget":
+          viewTotalBudget();
+          break;
 
         case "Exit":
           db.end();
           break;
       }
-     
     });
 };
 
 const viewAllDepartments = async () => {
-    console.log('Selecting all departments...\n');
-    try {
-      const [rows, fields] = await db.execute('SELECT * FROM department');
-      console.table(rows);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
-  };
+  console.log("Selecting all departments...\n");
+  try {
+    const [rows, fields] = await db.execute("SELECT * FROM department");
+    console.table(rows);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
+  }
+};
 
 const viewAllRoles = async () => {
-    console.log('Selecting all roles...\n');
-    try {
-      const [rows, fields] = await db.execute('SELECT * FROM role');
-      console.table(rows);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
+  console.log("Selecting all roles...\n");
+  try {
+    const [rows, fields] = await db.execute("SELECT * FROM role");
+    console.table(rows);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
   }
+};
 
 const viewAllEmployees = async () => {
-    console.log('Selecting all employees...\n');
-    try {
-      const [rows, fields] = await db.execute('SELECT * FROM employee');
-      console.table(rows);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
+  console.log("Selecting all employees...\n");
+  try {
+    const [rows, fields] = await db.execute("SELECT * FROM employee");
+    console.table(rows);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
   }
+};
 
-  const addDepartment = async () => {
-    console.log('Adding a department...\n');
-    const department = await inquirer.prompt({
-      name: 'name',
-      type: 'input',
-      message: 'What is the name of the department?',
-    });
-    try {
-      const [rows, fields] = await db.execute('INSERT INTO department (name) VALUES (?)', [department.name]);
-      console.log(`Added department with id ${rows.insertId}`);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
-  };
+const addDepartment = async () => {
+  console.log("Adding a department...\n");
+  const department = await inquirer.prompt({
+    name: "name",
+    type: "input",
+    message: "What is the name of the department?",
+  });
+  try {
+    const [rows, fields] = await db.execute(
+      "INSERT INTO department (name) VALUES (?)",
+      [department.name]
+    );
+    console.log(`Added department with id ${rows.insertId}`);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
+  }
+};
 
 const addRole = async () => {
-    console.log('Adding a role...\n');
-    const role = await inquirer.prompt([
-        {
-            name: 'title',
-            type: 'input',
-            message: 'What is the title of the role?',
-        },
-        {
-            name: 'salary',
-            type: 'number',
-            message: 'What is the salary of the role?',
-        },
-        {
-            name: 'department_id',
-            type: 'number',
-            message: 'What is the department id of the role?',
-        },
-        ]);
-    try {
-      const [rows, fields] = await db.execute('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [role.title, role.salary, role.department_id]);
-      console.log(`Added role with id ${rows.insertId}`);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
+  console.log("Adding a role...\n");
+  const role = await inquirer.prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What is the title of the role?",
+    },
+    {
+      name: "salary",
+      type: "number",
+      message: "What is the salary of the role?",
+    },
+    {
+      name: "department_id",
+      type: "number",
+      message: "What is the department id of the role?",
+    },
+  ]);
+  try {
+    const [rows, fields] = await db.execute(
+      "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+      [role.title, role.salary, role.department_id]
+    );
+    console.log(`Added role with id ${rows.insertId}`);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
   }
+};
 
 const addEmployee = async () => {
-    console.log('Adding an employee...\n');
-    const employee = await inquirer.prompt([
-        {
-            name: 'first_name',
-            type: 'input',
-            message: 'What is the first name of the employee?',
-        },
-        {
-            name: 'last_name',
-            type: 'input',
-            message: 'What is the last name of the employee?',
-        },
-        {
-            name: 'role_id',
-            type: 'number',
-            message: 'What is the role id of the employee?',
-        },
-        {
-            name: 'manager_id',
-            type: 'number',
-            message: 'What is the manager id of the employee?',
-        },
-        ]);
-    try {
-      const [rows, fields] = await db.execute('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [employee.first_name, employee.last_name, employee.role_id, employee.manager_id]);
-      console.log(`Added employee with id ${rows.insertId}`);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
+  console.log("Adding an employee...\n");
+  const employee = await inquirer.prompt([
+    {
+      name: "first_name",
+      type: "input",
+      message: "What is the first name of the employee?",
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "What is the last name of the employee?",
+    },
+    {
+      name: "role_id",
+      type: "number",
+      message: "What is the role id of the employee?",
+    },
+    {
+      name: "manager_id",
+      type: "number",
+      message: "What is the manager id of the employee?",
+    },
+    
+  ]);
+  try {
+    const [rows, fields] = await db.execute(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+      [
+        employee.first_name,
+        employee.last_name,
+        employee.role_id,
+        employee.manager_id,
+      ]
+    );
+    console.log(`Added employee with id ${rows.insertId}`);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
   }
+};
 
 const updateEmployeeRole = async () => {
-    console.log('Updating an employee role...\n');
-    const employee = await inquirer.prompt([
-        {
-            name: 'id',
-            type: 'number',
-            message: 'What is the id of the employee?',
-        },
-        {
-            name: 'role_id',
-            type: 'number',
-            message: 'What is the role id of the employee?',
-        },
-        ]);
-    try {
-      const [rows, fields] = await db.execute('UPDATE employee SET role_id = ? WHERE id = ?', [employee.role_id, employee.id]);
-      console.log(`Updated employee with id ${rows.insertId}`);
-      start();
-    } catch (error) {
-      console.error(`Error: ${error.toString()}`);
-    }
+  console.log("Updating an employee role...\n");
+  const employee = await inquirer.prompt([
+    {
+      name: "id",
+      type: "number",
+      message: "What is the id of the employee?",
+    },
+    {
+      name: "role_id",
+      type: "number",
+      message: "What is the role id of the employee?",
+    },
+  ]);
+  try {
+    const [rows, fields] = await db.execute(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [employee.role_id, employee.id]
+    );
+    console.log(`Updated employee with id ${rows.insertId}`);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
   }
+};
 
-    const EmployeesByManager = async () => {
-        console.log('Selecting all employees by manager...\n');
-        const employee = await inquirer.prompt([
-            {
-                name: 'manager_id',
-                type: 'number',
-                message: 'What is the manager id of the employee?',
-            },
-            ]);
-        try {
-          const [rows, fields] = await db.execute('SELECT * FROM employee WHERE manager_id = ?', [employee.manager_id]);
-          console.table(rows);
-          start();
-        } catch (error) {
-          console.error(`Error: ${error.toString()}`);
-        }
+const EmployeesByManager = async () => {
+  console.log("Selecting all employees by manager...\n");
+  const employee = await inquirer.prompt([
+    {
+      name: "manager_id",
+      type: "number",
+      message: "What is the manager id of the employee?",
+    },
+  ]);
+  try {
+    const [rows, fields] = await db.execute(
+      "SELECT * FROM employee WHERE manager_id = ?",
+      [employee.manager_id]
+    );
+    console.table(rows);
+    start();
+  } catch (error) {
+    console.error(`Error: ${error.toString()}`);
+  }
+};
+
+//View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+    const viewTotalBudget = async () => {  
+      console.log("Viewing total budget...\n");
+      const department = await inquirer.prompt([
+        {
+          name: "department_id",
+          type: "number",
+          message: "What is the department id?",
+        },
+      ]);
+      try {
+        const [rows, fields] = await db.execute(
+          "SELECT SUM(salary) FROM role WHERE department_id = ?",
+          [department.department_id]
+        );
+        console.table(rows);
+        start();
+      } catch (error) {
+        console.error(`Error: ${error.toString()}`);
       }
+    }
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
